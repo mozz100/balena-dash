2c083298db252b6b1699c75585405346ff0ea171
Richard Morrison <richard@rmorrison.net>
2021-11-24 20:53:18 +0000

    Send power to influxdb

--- currentcost/main.py
+++ currentcost/main.py
@@ -7,6 +7,8 @@ import signal
 import sys
 import time
 from xml.etree.cElementTree import fromstring
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 import requests
 import serial
@@ -15,13 +17,17 @@ serial = serial.Serial('/dev/ttyUSB0', 57600)
 MOZZWORLD_AUTH_TOKEN = os.environ.get("MOZZWORLD_AUTH_TOKEN")
 MOZZWORLD_URL_CURRENTCOST = os.environ.get("MOZZWORLD_URL_CURRENTCOST")
 
+INFLUXDB_TOKEN = os.environ.get("INFLUXDB_TOKEN")
+INFLUXDB_ORG = os.environ.get("INFLUXDB_ORG")
+INFLUXDB_BUCKET = os.environ.get("INFLUXDB_BUCKET")
+INFLUXDB_URL = os.environ.get("INFLUXDB_URL", "https://eu-central-1-1.aws.cloud2.influxdata.com")
+INFLUXDB_MEASUREMENT_BATCH = int(os.environ.get("INFLUXDB_MEASUREMENT_BATCH", "10"))
+
 class UTC(datetime.tzinfo):
     def utcoffset(self, dt):
         return datetime.timedelta(0)
-
     def tzname(self, dt):
         return "UTC"
-
     def dst(self, dt):
         return datetime.timedelta(0)
 
@@ -32,8 +38,29 @@ def utc_now_string():
 def signal_term_handler(signal, frame):
     sys.exit(0)
 
+def send_to_influxdb(measurements):
+    print("send_to_influxdb")
+    print(measurements)
+    sequence = []
+    for atime, watts in measurements:
+        ts = atime.timestamp() * 1000000000
+        line = f"power watts={watts} {ts}"
+        sequence.append(line)
+        print(line)
+    try:
+        with InfluxDBClient(
+            url=INFLUXDB_URL,
+            token=INFLUXDB_TOKEN,
+            org=INFLUXDB_ORG
+        ) as client:
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+            write_api.write(INFLUXDB_BUCKET, INFLUXDB_ORG, sequence)
+    except Exception as e:
+        print(e)
+
 signal.signal(signal.SIGTERM, signal_term_handler)
 
+measurements = []
 while True:
     msg = serial.readline()
     if not msg:
@@ -46,6 +73,7 @@ while True:
     watts = int(xml.find('ch1').find('watts').text)
     timestamp = utc_now_string()
     row = [timestamp, watts]
+    measurements.append([datetime.datetime.now(UTC()), watts])
     print(row)
     try:
         resp = requests.post(
@@ -59,3 +87,7 @@ while True:
         )
     except requests.HTTPError as e:
         print("HTTPError", e)
+    
+    if len(measurements) >= INFLUXDB_MEASUREMENT_BATCH:
+        send_to_influxdb(measurements)
+        measurements = []
